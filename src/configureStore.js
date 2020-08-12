6d79a38e5267abcdc7140692cfde9c0d6272f054
grwang <53140990+grwang91@users.noreply.github.com>
2020-08-13 00:56:47 +0900

    redux-persist 적용

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,8 +1,20 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers/rootReducer";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const enhancedReducer = persistReducer(persistConfig, rootReducer);
 
 function configureStore() {
-  return createStore(rootReducer);
+  const store = createStore(enhancedReducer);
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 }
 
 export default configureStore;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
-//import configureStore from "./store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
